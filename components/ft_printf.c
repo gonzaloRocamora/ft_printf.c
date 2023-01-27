994ad98458318084eb4a074d3641e49216d8adf3
Gonzalo Rocamora <67333112+gonzaloRocamora@users.noreply.github.com>
2023-01-27 10:44:27 +0100

    Update ft_printf.c

--- components/ft_printf.c
+++ components/ft_printf.c
@@ -12,53 +12,54 @@
 
 #include "ft_printf.h"
 
-int	ft_specifier(va_list args, const char format)
+int	ft_printchar(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_formats(va_list args, const char format)
 {
 	int	print_length;
 
 	print_length = 0;
 	if (format == 'c')
-		print_length += ft_putchar(va_arg(args, int));
+		print_length += ft_printchar(va_arg(args, int));
 	else if (format == 's')
-		print_length += ft_putstr(va_arg(args, char *));
+		print_length += ft_printstr(va_arg(args, char *));
 	else if (format == 'p')
 		print_length += ft_print_ptr(va_arg(args, unsigned long long));
 	else if (format == 'd' || format == 'i')
-		print_length += ft_putnbr(va_arg(args, int));
+		print_length += ft_printnbr(va_arg(args, int));
 	else if (format == 'u')
-		print_length += ft_print_uns(va_arg(args, unsigned int));
+		print_length += ft_print_unsigned(va_arg(args, unsigned int));
 	else if (format == 'x' || format == 'X')
-		print_length += ft_printhex(va_arg(args, unsigned int), format);
+		print_length += ft_print_hex(va_arg(args, unsigned int), format);
 	else if (format == '%')
-		print_length += ft_print_module();
+		print_length += ft_printpercent();
 	return (print_length);
 }
 
-int	ft_printf(char const *str, ...)
+int	ft_printf(const char *str, ...)
 {
+	int		i;
 	va_list	args;
-	char	*newstr;
-	size_t	i;
-	int		printed;
+	int		print_length;
 
 	i = 0;
-	printed = 0;
-	newstr = (char *)str;
+	print_length = 0;
 	va_start(args, str);
-	while (newstr[i])
+	while (str[i])
 	{
-		if (newstr[i] == '%')
+		if (str[i] == '%')
 		{
-			i++;
-			printed += ft_specifier(args, newstr[i]);
+			print_length += ft_formats(args, str[i + 1]);
 			i++;
 		}
 		else
-		{
-			printed += ft_putchar(newstr[i]);
-			i++;
-		}
+			print_length += ft_printchar(str[i]);
+		i++;
 	}
 	va_end(args);
-	return (printed);
-}
+}	
+	
